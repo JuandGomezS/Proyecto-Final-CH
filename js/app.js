7c69ab128c304cce1522ddc5951d38d4460223e7
Juan_GomezS <75592506+LYUJuanete@users.noreply.github.com>
2021-05-19 17:01:14 -0500

    Desafios arrays y primera entrega proyecto

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ enlaces.forEach((element) => {
 });
 
 window.onload = () => {
-  
+  console.clear();
   const hash = window.location.hash;
   if (hash === "") {
     setLink(enlaces[0]);
@@ -44,90 +44,266 @@ window.onload = () => {
       }
     });
   }
-}
+};
 
 //Clase para construir los objetos
 class Producto {
-  constructor(nombre, precio, iva, descuento) {
-      this.nombre  = nombre.toUpperCase();
-      this.precio  = parseFloat(precio);
-      this.iva = parseFloat(iva);
-      this.descuento = parseFloat(descuento);
+  constructor(tipo, nombre, precio, iva, descuento) {
+    this.tipo = tipo.toUpperCase();
+    this.nombre = nombre.toUpperCase();
+    this.price = parseFloat(precio);
+    this.precio = parseFloat(precio);
+    this.iva = parseFloat(iva);
+    this.descuento = parseFloat(descuento);
   }
 
   //funcion para calcular el precio al aumentar el iva
   sumaIva() {
-      this.precio = this.precio * (1 + (this.iva/100));
+    this.precio = this.precio * (1 + this.iva / 100);
   }
 
   descontar() {
-    this.precio = this.precio * ((this.descuento/100));
+    this.precio = this.precio * (this.descuento / 100);
+  }
+
+  restablecerPrecio() {
+    this.precio = this.price;
   }
 }
 
+let Productos = [];
+
+//productos de estampados
+const camisetas = new Producto("est", "Camisetas", "5000", "19", "0");
+const posillos = new Producto("est", "Posillos", "2000", "19", "0");
+const gorras = new Producto("est", "Gorras", "3000", "19", "0");
+const platos = new Producto("est", "Platos", "2000", "19", "0");
+
+//productos de fotografía
+const fotografiaD = new Producto("pho", "Fotografía digital", "5000", "19", "0");
+const fotografiaI = new Producto("pho", "Fotografía Impresa", "7000", "19", "0");
+
+//productos de video
+const videoGr = new Producto("vid", "Video Digital", "25000", "19", "0");
+const videoEd= new Producto("vid", "Video Digital", "30000", "19", "0");
+const videoGe = new Producto("vid", "Video Digital", "50000", "19", "0");
+
+//Publicidad
+const publicidadB = new Producto("ad", "Branding", "50000", "19", "0");
+const publicidadSN = new Producto("ad", "Redes Sociales", "100000", "19", "0");
+
+Productos.push(camisetas, posillos, gorras, platos, fotografiaD, fotografiaI, videoGr,videoEd, videoGe, publicidadB, publicidadSN);
 
 
-let preciosProductosEst = [];
 
 
 
-console.table(preciosProductosEst);
+//FUNCIONALIDAD PARA ESTAMPADOS
+document.getElementById("est").onclick = () => {
+  console.clear();
+  console.table(Productos);
+  let productosEst = Productos.filter(elemento => elemento.tipo === "EST");
+  console.table(productosEst);
+  
+  productosEst.sort(function (a, b) {
+    if (a.precio < b.precio){
+      return 1;
+    }
+    if (a.precio > b.precio){
+      return -1;
+    }
+    return 0;
+  });
+  console.table(productosEst);
+};
 
-document.getElementById("est-shirts").onclick= () => {
-  const camisetas = new Producto("Camisetas","5000","19","0");
-  preciosProductosEst.push(camisetas);
+document.getElementById("est-shirts").onclick = () => {
   let cantidad = parseInt(prompt(`¿Cuántas ${camisetas.nombre.toLowerCase()} quieres estampar?`));
   camisetas.sumaIva();
-  
-  let myModalEl = document.getElementById('estampados1')
-  let modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
-  modal.hide();
+  if (isNaN(cantidad)){
+    alert(`El costo es de: $ ${0}`);
+  }
+  else{
+    alert(`El costo es de: $ ${cantidad * camisetas.precio}`);
+  }
+  camisetas.restablecerPrecio();
+};
 
-  alert(`El costo es de: $ ${cantidad*camisetas.precio}`);
-  
-} 
-
-document.getElementById("est-cup").onclick= () => {
-  const posillos = new Producto("Posillos","2000","19","0");
-  preciosProductosEst.push(posillos);
+document.getElementById("est-cup").onclick = () => {
   let cantidad = parseInt(prompt(`¿Cuántas ${posillos.nombre.toLowerCase()} quieres estampar?`));
   posillos.sumaIva();
-  
-  let myModalEl = document.getElementById('estampados1')
-  let modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
-  modal.hide();
+  if (isNaN(cantidad)){
+    alert(`El costo es de: $ ${0}`);
+  }
+  else{
+    alert(`El costo es de: $ ${cantidad * posillos.precio}`);
+  }
+  posillos.restablecerPrecio();
+};
 
-  alert(`El costo es de: $ ${cantidad*posillos.precio}`);
-} 
-  
-document.getElementById("est-cap").onclick= () => {
-  const gorras = new Producto("Gorras","3000","19","0");
-  preciosProductosEst.push(gorras);
+document.getElementById("est-cap").onclick = () => {
   let cantidad = parseInt(prompt(`¿Cuántas ${gorras.nombre.toLowerCase()} quieres estampar?`));
   gorras.sumaIva();
-  
-  let myModalEl = document.getElementById('estampados1')
-  let modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
-  modal.hide();
-  
-  alert(`El costo es de: $ ${cantidad*gorras.precio}`);
-} 
+  if (isNaN(cantidad)){
+    alert(`El costo es de: $ ${0}`);
+  }
+  else{
+    alert(`El costo es de: $ ${cantidad * gorras.precio}`);
+  }
+  gorras.restablecerPrecio();
+};
 
-document.getElementById("est-dish").onclick= () => {
-  const platos = new Producto("Platos","2000","19","0");
-  preciosProductosEst.push(platos);
+document.getElementById("est-dish").onclick = () => {
   let cantidad = parseInt(prompt(`¿Cuántas ${platos.nombre.toLowerCase()} quieres estampar?`));
-  platos.sumaIva();
+  platos.sumaIva();  
+  if (isNaN(cantidad)){
+    alert(`El costo es de: $ ${0}`);
+  }
+  else{
+    alert(`El costo es de: $ ${cantidad * platos.precio}`);
+  }
+  platos.restablecerPrecio();
+};
+
+// FUNCIONALIDAD PARA FOTOGRAFÍA
+document.getElementById("foto").onclick = () => {
+  console.clear();
+  console.table(Productos);
+  let productosPh = Productos.filter(elemento => elemento.tipo === "PHO");
+  console.table(productosPh);
   
-  let myModalEl = document.getElementById('estampados1')
-  let modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
-  modal.hide();
+  productosPh.sort(function (a, b) {
+    if (a.precio < b.precio){
+      return 1;
+    }
+    if (a.precio > b.precio){
+      return -1;
+    }
+    return 0;
+  });
+  console.table(productosPh);
+};
+
+document.getElementById("foto-d").onclick = () => {
+  let cantidad = parseInt(prompt(`¿Cuántas ${fotografiaD.nombre.toLowerCase()} deseas?`));
+  fotografiaD.sumaIva();
+  if (isNaN(cantidad)){
+    alert(`El costo es de: $ ${0}`);
+  }
+  else{
+    alert(`El costo es de: $ ${cantidad * fotografiaD.precio}`);
+  }
+  fotografiaD.restablecerPrecio();
+};
+
+document.getElementById("foto-i").onclick = () => {
+  let cantidad = parseInt(prompt(`¿Cuántas ${fotografiaI.nombre.toLowerCase()} deseas?`));
+  fotografiaI.sumaIva();
+  if (isNaN(cantidad)){
+    alert(`El costo es de: $ ${0}`);
+  }
+  else{
+    alert(`El costo es de: $ ${cantidad * fotografiaI.precio}`);
+  }
+  fotografiaI.restablecerPrecio();
+};
+
+// FUNCIONALIDAD PARA VIDEO
+document.getElementById("video").onclick = () => {
+  console.clear();
+  console.table(Productos);
+  let productosVi = Productos.filter(elemento => elemento.tipo === "VID");
+  console.table(productosVi);
   
-  alert(`El costo es de: $ ${cantidad*platos.precio}`);
-} 
+  productosVi.sort(function (a, b) {
+    if (a.precio < b.precio){
+      return 1;
+    }
+    if (a.precio > b.precio){
+      return -1;
+    }
+    return 0;
+  });
+  console.table(productosVi);
+};
 
+document.getElementById("video-gr").onclick = () => {
+  let cantidad = parseInt(prompt(`¿Cuántos minutos de ${videoGr.nombre.toLowerCase()} en grabación deseas?`));
+  videoGr.sumaIva();
+  if (isNaN(cantidad)){
+    alert(`El costo es de: $ ${0}`);
+  }
+  else{
+    alert(`El costo es de: $ ${cantidad * videoGr.precio}`);
+  }
+  videoGr.restablecerPrecio();
+};
 
+document.getElementById("video-ed").onclick = () => {
+  let cantidad = parseInt(prompt(`¿Cuántos minutos de ${videoEd.nombre.toLowerCase()} en edición deseas?`));
+  videoEd.sumaIva();
+  if (isNaN(cantidad)){
+    alert(`El costo es de: $ ${0}`);
+  }
+  else{
+    alert(`El costo es de: $ ${cantidad * videoEd.precio}`);
+  }
+  videoEd.restablecerPrecio();
+};
+
+document.getElementById("video-ge").onclick = () => {
+  let cantidad = parseInt(prompt(`¿Cuántos minutos de ${videoGe.nombre.toLowerCase()} en edición y grabación deseas?`));
+  videoGe.sumaIva();
+  if (isNaN(cantidad)){
+    alert(`El costo es de: $ ${0}`);
+  }
+  else{
+    alert(`El costo es de: $ ${cantidad * videoGe.precio}`);
+  }
+  videoGe.restablecerPrecio();
+};
+
+// FUNCIONALIDAD PARA PUBLICIDAD
+document.getElementById("ads").onclick = () => {
+  console.clear();
+  console.table(Productos);
+  let productosAd = Productos.filter(elemento => elemento.tipo === "AD");
+  console.table(productosAd);
   
+  productosAd.sort(function (a, b) {
+    if (a.precio < b.precio){
+      return 1;
+    }
+    if (a.precio > b.precio){
+      return -1;
+    }
+    return 0;
+  });
+  console.table(productosAd);
+};
 
+document.getElementById("pub-b").onclick = () => {
+  let cantidad = parseInt(prompt(`¿Cuántos servicios de ${publicidadB.nombre.toLowerCase()} deseas?`));
+  publicidadB.sumaIva();
+  if (isNaN(cantidad)){
+    alert(`El costo es de: $ ${0}`);
+  }
+  else{
+    alert(`El costo es de: $ ${cantidad * publicidadB.precio}`);
+  }
+  publicidadB.restablecerPrecio();
+};
 
-
+document.getElementById("pub-sn").onclick = () => {
+  let cantidad = parseInt(prompt(`¿Cuántos servicios de publicidad en ${publicidadSN.nombre.toLowerCase()} deseas?`));
+  console.log(cantidad)
+  publicidadSN.sumaIva();
+  if (isNaN(cantidad)){
+    alert(`El costo es de: $ ${0}`);
+  }
+  else{
+    alert(`El costo es de: $ ${cantidad * publicidadSN.precio}`);
+  }
+  
+  publicidadSN.restablecerPrecio();
+};
